a1a515c39750fbff430dcbabd75395954353b0ee
jspradd57 <jspradd@gmail.com>
2025-10-29 16:12:34 -0400

    Add files via upload

--- ShiftLift/src/main/java/se300/shiftlift/UserRepository.java
+++ ShiftLift/src/main/java/se300/shiftlift/UserRepository.java
@@ -1,14 +1,21 @@
 package se300.shiftlift;
 
+import java.util.List;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
 
 interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
     //Note that extending JpaRepository gives UserRepository a lot of built in methods already
     
     // If you don't need a total row count, Slice is better than Page as it only performs a select query.
     // Page performs both a select and a count query.
+
+    //Returns a slice of all Users in the datbase
     Slice<User> findAllBy(Pageable pageable);
+    //Returns a list of all users with a given Username
+    List<User> findByEmail(String email);
 }
